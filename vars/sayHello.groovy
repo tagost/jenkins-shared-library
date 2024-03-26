c74f4ba956ecd2b90a8b3dc9dca88cdf95ab2b51
tagost <guillesos@hotmial.es>
2024-03-26 14:28:25 -0400

    asdf

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,20 @@
 #!/usr/bin/env groovy
 def call(String name = 'human') {
-  echo "Hello, ${name}."
+  pipeline {
+  agent none
+  stages {
+    stage('Deploy test') {
+      when {
+        expression {
+          BRANCH_NAME =~ 'okd-develop*'
+        }
 
+      }
+      steps {
+        sh "echo ${BRANCH_NAME}"
+      }
+    }
+
+  }
+}
 }
