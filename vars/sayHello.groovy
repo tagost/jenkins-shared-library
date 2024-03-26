c0525cbed5175097aae12dca7072f18fa68d766d
tagost <guillesos@hotmial.es>
2024-03-26 14:53:15 -0400

    asd

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -6,11 +6,15 @@ def call(String name = 'human') {
     stage('Deploy test') {
       when {
         expression {
-          BRANCH_NAME =~ 'okd-develop*'
+          BRANCH_NAME =~ 'main'
         }
 
       }
+      environment{
+        COMMIT="1234565"
+      }
       steps {
+        sh "echo ${BRANCH_NAME}"
         sh "echo ${BRANCH_NAME}"
       }
     }
